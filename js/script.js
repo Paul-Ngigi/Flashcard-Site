a58c588cc3dfd7fb57aaea19223c425a1048d9f8
Paul-Ngigi <paulkush7777@gmail.com>
2021-02-24 14:02:31 +0300

    add a toggle effect for the javascript abbreviation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     console.log("Content Loaded");
+
+    $('.js-clickable').click(function(){
+        $('#js-hidden').slideToggle();
+        $('#js-visible').slideToggle();
+    });
 });
